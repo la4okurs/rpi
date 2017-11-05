700b1cfc60c31346f48f8b786cc4e1eb8010a41d
la4okurs <33391448+la4okurs@users.noreply.github.com>
2017-11-05 22:59:06 +0100

    Create usingtemp.py

---
+++ usingtemp.py
@@ -0,0 +1,38 @@
+#!/bin/env/python
+# -*- coding: utf-8 -*-
+#
+# TEXT AREA
+# Dette programmet (denne filen) skal du kalle usingtemp.py
+# og dette prammet skal kalle begge funksjonene du laget i temperature.py programmet
+# ved hjelp av import (module) i Python
+#
+# Slik prorammet nå er skrevet vil det feile.
+# Importer riktig under og få programmet til å kjøre
+# Hint: Du trenger ikke å tilføre flere statements, kun rette opp de statements som
+# er gitt
+#
+# PS: la begge programmene temperature.py og usingtemp.py ligge på samme directory
+#     Det er da enklere å kalle uten at file PATH oppgis
+#
+
+# IMPORT AREA
+import sys 
+import .....
+
+
+# GLOBAL DATA AREA
+# a = 3 # substitute a or other data to be your own data
+#
+
+# FUNCTION AREA
+# change the 'pass' statement below to be your own statements
+
+
+# MAIN AREA
+if __name__ == "__main__":
+
+   temp.calc_fahrenheit()
+   temp.calc_celsius()  # gir feilmelding, du må rette opp kallet selv her
+
+   sys.exit(0)
+

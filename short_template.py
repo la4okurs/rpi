fcf6e76ae6f99a54962980501a611dcc6463a75f
la4okurs <33391448+la4okurs@users.noreply.github.com>
2017-11-05 13:19:45 +0100

    Create short_template.py

---
+++ short_template.py
@@ -0,0 +1,24 @@
+#!/bin/env/python
+# -*- coding: utf-8 -*-
+#
+# IMPORT AREA
+import sys 
+
+
+# GLOBAL DATA AREA
+# a = 3 # substitute a or other data to be your own data
+#
+
+# FUNCTION AREA
+def foo():
+   pass # substitute this to be your own function statements
+
+
+# MAIN AREA
+if __name__ == "__main__":
+   foo() # call your function(s) here
+   # ....
+   # ...
+
+
+   sys.exit(0)

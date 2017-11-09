ab19fcf0805324f6e7d7df86e3edb00923430b13
la4okurs <33391448+la4okurs@users.noreply.github.com>
2017-11-09 10:02:40 +0100

    Create usingtemp.py

---
+++ svar/usingtemp.py
@@ -0,0 +1,20 @@
+#!/bin/env/python
+# -*- coding: utf-8 -*-
+#
+# TEXT AREA
+# Dette programmet (denne filen) skal du kalle usingtemp.py
+# og dette prammet skal kalle begge funksjonene du laget i temperature.py programmet
+# ved hjelp av import (module) i Python
+
+# IMPORT AREA
+import sys 
+import temperature   # import functions defined in the temperature.py
+
+# MAIN AREA
+if __name__ == "__main__":
+
+   temperature.calc_fahrenheit()
+   temperature.calc_celsius()
+
+   sys.exit(0)
+

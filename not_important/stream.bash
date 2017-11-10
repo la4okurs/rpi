d7a15d526a385eedc8e5d68243eedac395098696
la4okurs <33391448+la4okurs@users.noreply.github.com>
2017-11-10 13:14:45 +0100

    Update stream.bash

--- not_important/stream.bash
+++ not_important/stream.bash
@@ -1,7 +1,7 @@
+
 #/bin/bash
-#set -x
 VLC_LISTENVHF_PATH="/home/pi/rpi"
-MYLIST="nrk p4 ham john"
+MYLIST="nrk p4 ham john status stop"
 while true;do
    if [ $# -eq 0 ];then
       echo "Select one of the following:"
@@ -43,10 +43,19 @@ elif [ "$ARG" = "p4" ];then
    # bash ${VLC_LISTENVHF_PATH}/vlc_listenvhf.bash http://streaming.radio.co/s9fa0dff72/listen  start &
    bash ${VLC_LISTENVHF_PATH}/vlc_listenvhf.bash http://stream.p4.no/p4_mp3_hq start
 elif [ "$ARG" = "nrk" ];then
-   bash ${VLC_LISTENVHF_PATH}/vlc_listenvhf.bash http://nrk-mms-live.telenorcdn.net:80/nrk_radio_p13_aac_h start
+   bash ${VLC_LISTENVHF_PATH}/vlc_listenvhf.bash http://nrk-mms-live.telenorcdn.net:80/nrk_radio_p13_aac_h start &
 elif [ "$ARG" = "ham" ];then
-   bash ${VLC_LISTENVHF_PATH}/vlc_listenvhf.bash start
+   bash ${VLC_LISTENVHF_PATH}/vlc_listenvhf.bash start &
+elif [ "$ARG" = "status" ];then
+   bash ${VLC_LISTENVHF_PATH}/vlc_listenvhf.bash status
+elif [ "$ARG" = "stop" ];then
+   bash ${VLC_LISTENVHF_PATH}/vlc_listenvhf.bash stop
 else
    :
 fi
 exit 0
+
+
+
+
+

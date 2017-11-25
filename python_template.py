e3775b0fd4a281bdd758034b234a1825a8b9dbb9
la4okurs <33391448+la4okurs@users.noreply.github.com>
2017-11-25 23:40:58 +0100

    Create python_template.py

---
+++ python_template.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# TEXT AREA explaining what the program is doing, any copyrights, licenses, etc
+# Copyright 2017 MyLittleCompany
+# Licensed under the ... License, Version X.X
+#
+# This program will do..
+#
+# Author:
+#
+
+
+# IMPORT AREA
+import sys 
+
+
+# GLOBAL DATA AREA
+# a = 3 # substitute a or other data to be your own data
+
+
+
+# FUNCTION AREA
+def foo():
+   pass # substitute this to be your own function statements
+
+
+
+
+# MAIN AREA
+if __name__ == "__main__":
+   foo() # call your function(s) here
+   # ....
+   # ...
+
+   
+   sys.exit(0) # All exits back to outer shell should leave 0 if no error(s) and >< 0 if failing

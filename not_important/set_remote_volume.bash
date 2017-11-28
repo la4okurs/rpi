8e34ee3f43151390dd7f25572cb7bbf2f4dea14f
la4okurs <33391448+la4okurs@users.noreply.github.com>
2017-11-28 14:05:58 +0100

    Update set_remote_volume.bash

--- not_important/set_remote_volume.bash
+++ not_important/set_remote_volume.bash
@@ -18,7 +18,7 @@ usage_exit() {
 
 setvolumblackboxRPI() {
    #DEVICES=$(amixer scontrols | grep "Simple mixer control" | awk -F "Simple mixer control" '{print $2}' | tr -d "'" | awk -F "," '{print $1}' | awk '{print $1}' | sort | uniq)
-   DEVICES=$(amixer controls | awk -F "'" '{print $2}' | grep Playback | awk '{print $1}' | sort | uniq)
+   DEVICES=$(amixer controls | awk -F "'" '{print $2}' | awk '{print $1}' | sort | uniq)
    DEVICE=""
    FOUND=0
    for i in $DEVICES;do

4a1dcbdd54204bb7ff909b3e990e4a2bf0a1e6b0
la4okurs <33391448+la4okurs@users.noreply.github.com>
2017-11-29 23:58:28 +0100

    Update gpio_in.py

--- svar/gpio_in.py
+++ svar/gpio_in.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
    while True:
       # read button 1 or 0 as result:
       i = GPIO.input(gpioport) # read level of the gpioport pin
-      print i                  # i gets the value 1 or 0 (GPIO.HIGH or GPIO.LOW)
+      print i                  # i gets the value 1 or 0
       time.sleep(0.2)          # sleep to offload the CPU
    
    sys.exit(0) # Exit 0 = success to outer bash shell transfer

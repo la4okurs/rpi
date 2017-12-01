698fb7c06ac66a32929f19de195bf7da10e47c68
la4okurs <33391448+la4okurs@users.noreply.github.com>
2017-12-01 11:37:18 +0100

    Create radio_button.bash

---
+++ not_important/radio_button.bash
@@ -0,0 +1,24 @@
+#!/bin/sh
+#
+# Autor: Steinar/LA7XQ
+#
+
+killscriptofthistype() {
+   # call like 'killscriptofthistype $$'
+   set -x
+   for i in $(pgrep -f $(basename $0));do
+      if [ ! "$i" = "$1" ];then
+         kill -9 $i > /dev/null 2>&1
+      fi
+   done
+}
+
+kill_earlier() {
+   kill -9 $(pgrep -f $1) >/dev/null 2>&1
+}
+
+
+killscriptofthistype $$
+kill_earlier gpio_in_callbacks_start_stream.py
+/usr/bin/python $HOME/rpi/not_important/gpio_in_callbacks_start_stream.py > /dev/null 2>&1 &
+echo

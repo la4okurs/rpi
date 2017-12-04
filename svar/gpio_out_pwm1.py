9861dedf1280aeaaab87eeb8fb2c5649f99b6f3b
la4okurs <33391448+la4okurs@users.noreply.github.com>
2017-12-04 10:48:46 +0100

    Create gpio_out_pwm1.py

---
+++ svar/gpio_out_pwm1.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# TEXT AREA explaining what the program is doing, any copyrights, licenses, etc
+# Copyright 2017 MyLittleCompany
+# Licensed under the ... License, Version X.X
+#
+# This program give a short blink based on the GPIO Pulse Width Modulation scheme
+# The example also use physical pin numbering scheme as BOARD pinning is used
+#
+# Author: Steinar/LA7XQ
+#
+
+
+# IMPORT AREA
+import RPi.GPIO as GPIO
+import sys 
+
+
+# GLOBAL DATA AREA
+GPIO.setmode(GPIO.BOARD)        # BOARD mens use physical pin scheme
+gpioport=22                     # here phys pin 22  (GPIO25)
+GPIO.setup(gpioport, GPIO.OUT)
+
+
+# FUNCTION AREA
+
+
+
+# MAIN AREA
+if __name__ == "__main__":
+   p = GPIO.PWM(gpioport, 0.5)  # prepare output pin for PWM with frequency 0.5 Hz 
+   p.start(2)                   # start with duty cycle 2 % (will cause a short blink)
+   raw_input("Press return to stop blinking:") # use input statement if using Python3
+   p.stop()                     # stop PWM
+
+   # proper GPIO cleanup as well as return to outer shell with no expected errors:
+   GPIO.cleanup()
+   sys.exit(0) # All exits back to outer shell should leave 0 if no error(s) and >< 0 if failing

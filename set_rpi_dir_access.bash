fca6cd560949a2a9488cc3953cc228afe6919790
la4okurs <33391448+la4okurs@users.noreply.github.com>
2018-04-25 14:48:18 +0200

    Update set_rpi_dir_access.bash

--- set_rpi_dir_access.bash
+++ set_rpi_dir_access.bash
@@ -1,12 +1,18 @@
 #!/bin/bash
+#
+# Author: Steinar/LA7XQ
+#
 
-# SWE: add next lines from next block to the .bashrc file ------
-# SWE: added lines to the .bashrc file stops ------
+# directions:
+# leave next commented block as a none commented code in the
+# last part of the $HOME/.bashrc   file :
+#
 #if [ -f $HOME/rpi/set_rpi_dir_access.bash ];then
 #   . $HOME/rpi/set_rpi_dir_access.bash
 #fi
-# SWE: added lines to the .bashrc file stops ------
-
+#
 # Do not copy the next lines to the $HOME/.bashrc  file:
-export PATH="$PATH:$HOME/rpi"
+if ! echo $PATH | grep -q -E "$HOME/rpi";then
+   export PATH="$PATH:$HOME/rpi"
+fi
 sudo chmod a+rx $HOME/rpi/get*

239a9483fb1369bdd96aea619e288b030bbbb87a
la4okurs <33391448+la4okurs@users.noreply.github.com>
2018-04-26 10:40:01 +0200

    Update set_rpi_dir_access.bash

--- set_rpi_dir_access.bash
+++ set_rpi_dir_access.bash
@@ -2,17 +2,31 @@
 #
 # Author: Steinar/LA7XQ
 #
+# This script adds code to the $HOME/.bashrc file
+# in order to set execute access on the $HOME/rpi/get*   files
+# and add the $HOME/rpi/  directory to the PATH environment
+#
 
-# directions:
-# leave next commented block as a none commented code in the
-# last part of the $HOME/.bashrc   file :
-#
-#if [ -f $HOME/rpi/set_rpi_dir_access.bash ];then
-#   . $HOME/rpi/set_rpi_dir_access.bash
-#fi
-#
-# Do not copy the next lines to the $HOME/.bashrc  file:
-if ! echo $PATH | grep -q -E "$HOME/rpi";then
-   export PATH="$PATH:$HOME/rpi"
+if [ ! -f $HOME/.bashrc ];then
+   mkdir -p $HOME/.bashrc
 fi
-sudo chmod a+rx $HOME/rpi/get*
+#ls -ld $HOME/.bashrc
+if ! cat $HOME/.bashrc | grep -q -E "if .*set_rpi_dir_access.bash";then
+   echo "# SWE added:" >> $HOME/.bashrc
+   echo "if [ -f \$HOME/rpi/set_rpi_dir_access.bash ];then" >> $HOME/.bashrc
+   echo ". \$HOME/rpi/set_rpi_dir_access.bash" >>$HOME/.bashrc
+   echo "fi" >>$HOME/.bashrc
+   echo "INFO: New last part of $HOME/.bashrc set as:"
+   cat $HOME/.bashrc | tail -n 5
+fi   
+
+# avoid appending multiple same directory $HOME/rpi to the final PATH:
+if ! echo $PATH | grep -q -E "$HOME/rpi";then
+   [ -d "$HOME/rpi" ] && export PATH="$PATH:$HOME/rpi"
+   sudo chmod a+rx $HOME/rpi/get*
+   echo "INFO:New PATH updated as '$PATH'"
+fi
+echo "INFO:"
+echo "You should now be able to type simply 'getip', 'getrpimodel' etc"
+echo "from any pi directory"
+exit 0

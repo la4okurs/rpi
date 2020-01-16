fce7eb5ee2d8e93b3a72a3873715858f96cc6869
la4okurs <33391448+la4okurs@users.noreply.github.com>
2020-01-16 12:20:19 +0100

    Create template_python3.py

---
+++ template_python3.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python       # you may substitute python with python3 as well
+#
+# COMMENT AREA
+#
+# Template made by Steinar W
+#
+# Usage:
+# In your program delete from this template the comment area starting with
+# and including the # COMMENT AREA line above, but be sure to
+# keep the very two first lines of this file and
+# KEEP ALL THE UNCOMMENTED LINES below as in this file!
+#
+# Making a new program:
+# When making a new program you may simply have a quick start of your
+# own source file by coping this file to your new yourprogra.py:
+#
+# $ cp thisfile.py  yourprogram.py      # e.g make a copy of the template
+#
+# and then adjust the yourprogram.py  file to be your own program
+#
+#
+# IMPORT AREA
+# import ....
+#
+
+
+
+#
+#
+# GLOBAL DATA AREA
+# a = 3 # (likewise)
+#
+
+
+
+#
+# FUNCTION AREA
+# change the 'pass' statement below to be your own statements
+def foo():
+   pass # substitute this to be your own function statements
+
+
+
+#
+# MAIN AREA
+if __name__ == "__main__":  # keeping this line helps you creating libraries
+   foo() # call your function(s) here
+   # ....
+   # ...
+
+
+   # In the end of the program use exit statement to indicate
+   # to outer shell (like bash) the success of running this program
+   exit(0)
+
